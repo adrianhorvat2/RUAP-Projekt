fe7078171d94574564db9689778cf31a199b8b27
adrianhorvat2 <adro.horvat@gmail.com>
2024-11-04 15:21:44 +0100

    Returned older version

--- RUAP/RUAP/Program.cs
+++ RUAP/RUAP/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Pozdrav iz aplikacije od Adriana Horvata, indeks:1234");
+
+            Console.WriteLine("Pozdrav iz aplikacije od Adriana Horvata");
+
         }
     }
 }
