61aeaa7bcc91fe0bb0f089cc4dd49935d4e1daad
adrianhorvat2 <adro.horvat@gmail.com>
2024-11-04 15:37:55 +0100

    Added index number

--- RUAP/RUAP/Program.cs
+++ RUAP/RUAP/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Pozdrav iz aplikacije od Adriana Horvata");
+            Console.WriteLine("Pozdrav iz aplikacije od Adriana Horvata, indeks:1234");
         }
     }
 }
